3367c9a2172d9f2a6854f1e2a39fe70c1e5da0da
PredaMihaiDragos <48451420+PredaMihaiDragos@users.noreply.github.com>
2020-04-03 15:32:44 +0300

    upd

--- Anul 1/TW/Proiect/app.js
+++ Anul 1/TW/Proiect/app.js
@@ -13,27 +13,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/get_pacient', async function(req, res) {
-    const x = await db_rez.GetPacient(req.query.cnp);
-
-    if(x.length > 0)
-        res.send(x[0]);
-    else
+app.get('/get_bilet', async function(req, res) {
+    const pacient = await db_rez.GetPacient(req.query.cnp);
+    if(pacient.length == 0)
     {
         res.status(404);      
         res.send('Pacientul nu a fost gasit.');
     }
-})
-
-app.get('/get_bilet', async function(req, res) {
-    const x = await db_rez.GetBilet(req.query.cnp, req.query.id);
-    if(x.length > 0)
-        res.send(x[0]);
-    else
+    const bilet = await db_rez.GetBilet(req.query.cnp, req.query.id);
+    if(bilet.length == 0)
     {
         res.status(404);      
         res.send('Biletul nu a fost gasit.');
     }
+    bilet[0].ANALIZE = JSON.parse(bilet[0].ANALIZE);
+    const analize = bilet[0].ANALIZE;
+    codgs = new Set();
+    for(let i = 0; i < analize.length; i++)
+        codgs.add(analize[i].aCodg);
+    const grupe = await db_rez.GetGrupe(Array.from(codgs));
+    const ret = {"pacient": pacient[0], "bilet":bilet[0], "grupe":grupe};
+    res.send(ret);
 })
 
 module.exports = app;
