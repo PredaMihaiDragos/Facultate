7978f05877088585542a2cf68b51e7a4df9afe25
PredaMihaiDragos <48451420+PredaMihaiDragos@users.noreply.github.com>
2020-04-22 12:49:22 +0300

    Final responsive mobil

--- Anul 1/TW/Proiect/public/js/modalBox.js
+++ Anul 1/TW/Proiect/public/js/modalBox.js
@@ -5,7 +5,20 @@ document.getElementById("chestionarClose").onclick = function() {
   HideChestionar();
 }
 
+document.getElementById("certificariClose").onclick = function() {
+  HideCertificare();
+}
+
 window.onclick = function(event) {
+  if (event.target == chestionarModal) {
+    HideChestionar();
+  }
+  else if(event.target == certificariModal) {
+    HideCertificare();
+  }
+}
+
+window.ontouchstart = function(event) {
   if (event.target == chestionarModal) {
     HideChestionar();
   }
@@ -24,17 +37,17 @@ function HideChestionar()
     chestionarModal.style.display = "none";
 }
 
+let certificareImg;
 function ShowCertificare(url)
 {
     const img = document.createElement("img");
     img.src = url;
-    certificariModal.appendChild(img);
+    certificareImg = certificariModal.appendChild(img);
     certificariModal.style.display = "block";
 }
 
 function HideCertificare()
 {
-    while(certificariModal.firstChild)
-      certificariModal.lastChild.remove();
+    certificareImg.remove();
     certificariModal.style.display = "none";
 }
