e8225f5eea35fca0e1982c9bcb61cbd57779389f
PredaMihaiDragos <48451420+PredaMihaiDragos@users.noreply.github.com>
2020-04-22 15:42:01 +0300

    Delete automaton to recreate

--- Anul 1/LFA/automaton/Source.cpp
+++
@@ -1,7 +0,0 @@
-#include "lambda_nfa.h"
-
-int main()
-{
-	lambda_nfa aut(5);
-	return 0;
-}
--- Anul 1/LFA/automaton/lambda_nfa.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "automaton.h"
-class lambda_nfa : public automaton
-{
-public:
-	lambda_nfa(int nr_stari = 0);
-	void AddEdge(int from, int to, char val);
-};
-
